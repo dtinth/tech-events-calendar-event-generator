2a05db49cad12b664b2397eacf32d5ed79e38290
Thai Pangsakulyanont <dtinth@spacet.me>
2018-02-21 01:17:18 +0700

    Playing around with primer css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
-  <div id="app">
-    Hello
+  <div id="app" class="p-4 container-md">
+    <h1 class="h1">Event file generator</h1>
+    <form class="mt-2">
+      <dl class="form-group">
+        <dt><label>Title</label></dt>
+        <dd>
+          <input class="form-control input-block input-lg" placeholder="Event title">
+        </dd>
+      </dl>
+      <dl class="form-group">
+        <dt><label>Summary</label></dt>
+        <dd>
+          <textarea class="form-control input-block" style="height: 4em; min-height: 4em;" placeholder="Event summary (one line, 280 chars)"></textarea>
+        </dd>
+      </dl>
+      <dl class="form-group">
+        <dt><label>Description</label></dt>
+        <dd>
+          <textarea class="form-control input-block" style="height: 7em; min-height: 7em;" placeholder="Event description"></textarea>
+        </dd>
+      </dl>
+    </form>
+    <div class="mt-4">
+      <div class="markdown-body">
+        <pre>Result</pre>
+      </div>
+    </div>
   </div>
 </template>
 
